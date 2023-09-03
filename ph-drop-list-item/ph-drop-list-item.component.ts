cb86ad9818d769d339a70103fc3b1dfdedd97d2d
Lucas Mösch <lucasmoesch@live.de>
2023-09-03 19:51:57 +0200

    added preliminary drop list components

---
+++ ph-drop-list-item/ph-drop-list-item.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'ph-drop-list-item',
+  templateUrl: './ph-drop-list-item.component.html',
+  styleUrls: ['./ph-drop-list-item.component.scss']
+})
+export class PhDropListItemComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
