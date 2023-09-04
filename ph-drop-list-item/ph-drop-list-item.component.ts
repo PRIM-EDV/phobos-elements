acd182181c9af37244d77716e1248814c15ecc37
Lucas Moesch <lucasmoesch@live.de>
2023-09-04 15:44:55 +0200

    Added drag and drop handles

--- ph-drop-list-item/ph-drop-list-item.component.ts
+++ ph-drop-list-item/ph-drop-list-item.component.ts
@@ -1,64 +1,67 @@
-import { Component, ElementRef, OnInit } from '@angular/core';
+import { Component, ElementRef, Input, OnInit } from "@angular/core";
+import { Subject } from "rxjs";
 
 @Component({
-  selector: 'ph-drop-list-item',
-  templateUrl: './ph-drop-list-item.component.html',
-  styleUrls: ['./ph-drop-list-item.component.scss']
+  selector: "ph-drop-list-item",
+  templateUrl: "./ph-drop-list-item.component.html",
+  styleUrls: ["./ph-drop-list-item.component.scss"],
 })
 export class PhDropListItemComponent implements OnInit {
 
-private dragStartCoursor = {x: 0, y: 0};
-private dragStartElement = {x: 0, y: 0};
+  @Input() data: any;
 
-public isDragged = false;
+  public onDragStart: Subject<any> = new Subject<any>();
+  public onDragStop: Subject<any> = new Subject<any>();
 
-// private mouseMoveListener: 
+  private dragStartCoursor = { x: 0, y: 0 };
+  private dragStartElement = { x: 0, y: 0 };
 
-  constructor(public ref: ElementRef) { 
+  constructor(public ref: ElementRef) {
     this.handleDrag = this.handleDrag.bind(this);
     this.handleDragStop = this.handleDragStop.bind(this);
   }
 
-  ngOnInit(): void {
-  }
+  ngOnInit(): void {}
 
   handleDragStart(ev: MouseEvent) {
     ev.preventDefault();
-    console.log("start")
+
+    this.onDragStart.next(this.data);
 
     const rect = this.ref.nativeElement.getBoundingClientRect();
-    this.dragStartCoursor = {x: ev.x, y: ev.y};
-    this.dragStartElement = {x: this.ref.nativeElement.offsetLeft, y: this.ref.nativeElement.offsetTop};
-    
+    this.dragStartCoursor = { x: ev.x, y: ev.y };
+    this.dragStartElement = {
+      x: this.ref.nativeElement.offsetLeft,
+      y: this.ref.nativeElement.offsetTop,
+    };
 
     this.ref.nativeElement.style.position = "absolute";
     this.ref.nativeElement.style.width = `${rect.width}px`;
     this.ref.nativeElement.style.height = `${rect.height}px`;
-    console.log(`${rect.width} px`)
+    this.ref.nativeElement.style.pointerEvents = "none";
+
     window.addEventListener("mousemove", this.handleDrag);
     window.addEventListener("mouseup", this.handleDragStop);
-
-    this.isDragged = true;
   }
 
   handleDrag(ev: MouseEvent) {
     ev.preventDefault();
 
-    if (this.isDragged) {
-        this.ref.nativeElement.style.left = `${this.dragStartElement.x + ev.x - this.dragStartCoursor.x}px`;
-        this.ref.nativeElement.style.top = `${this.dragStartElement.y + ev.y - this.dragStartCoursor.y}px`;
-    }
+    this.ref.nativeElement.style.left = `${ this.dragStartElement.x + ev.x - this.dragStartCoursor.x}px`;
+    this.ref.nativeElement.style.top = `${this.dragStartElement.y + ev.y - this.dragStartCoursor.y}px`;
   }
 
   handleDragStop() {
     window.removeEventListener("mousemove", this.handleDrag);
     window.removeEventListener("mouseup", this.handleDragStop);
 
+    this.onDragStart.next(this.data);
+
     this.ref.nativeElement.style.position = null;
     this.ref.nativeElement.style.width = null;
     this.ref.nativeElement.style.height = null;
     this.ref.nativeElement.style.left = null;
     this.ref.nativeElement.style.top = null;
+    this.ref.nativeElement.style.pointerEvents = null;
   }
-
 }
--- ph-drop-list/ph-drop-list.component.ts
+++ ph-drop-list/ph-drop-list.component.ts
@@ -1,17 +1,46 @@
-import { Component, Input, OnInit } from '@angular/core';
+import { AfterContentInit, Component, ContentChildren, EventEmitter, HostListener, Input, OnInit, Output, QueryList } from '@angular/core';
+import { PhDropListItemComponent } from '../ph-drop-list-item/ph-drop-list-item.component';
 
 @Component({
   selector: 'ph-drop-list',
   templateUrl: './ph-drop-list.component.html',
   styleUrls: ['./ph-drop-list.component.scss']
 })
-export class PhDropListComponent implements OnInit {
+export class PhDropListComponent implements OnInit, AfterContentInit {
 
   @Input() header: string = '';
+  @Input() connectedLists: Array<PhDropListComponent> = [];
+
+  @Output() drop: EventEmitter<any> = new EventEmitter<any>();
+
+  @ContentChildren(PhDropListItemComponent) itemComponents!: QueryList<PhDropListItemComponent>;
+
+//   public output.
+  public draggedItem: any;
 
   constructor() { }
 
   ngOnInit(): void {
   }
 
+  ngAfterContentInit(): void {
+    // this.itemComponents.changes();
+    for(const item of this.itemComponents) {
+      item.onDragStart.subscribe((item) => {this.draggedItem = item;});
+      item.onDragStop.subscribe()
+    }
+  }
+
+  @HostListener('mouseup', ['$event'])
+  onMouseUp(event: MouseEvent) {
+    console.log("UP?")
+    for (const list of this.connectedLists) {
+        console.log(this.connectedLists)
+        if (list.draggedItem != undefined) {
+            console.log("??")
+            this.drop.next(list.draggedItem);
+            return;
+        }
+    }
+  }
 }
