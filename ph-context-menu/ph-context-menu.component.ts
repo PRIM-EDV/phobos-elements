eb2ad951dbffc85aed8b4ceaaee66f275ae18431
Lucas Mösch <lucasmoesch@live.de>
2025-05-04 12:40:17 +0200

    Changed context menu to explizit integration

--- ph-context-menu/ph-context-menu.component.ts
+++ ph-context-menu/ph-context-menu.component.ts
@@ -1,44 +1,53 @@
-import { Component, ElementRef, HostBinding, OnInit } from '@angular/core';
+import {
+  AfterViewInit,
+  Component,
+  ElementRef,
+  Input,
+  OnInit,
+} from "@angular/core";
 
 @Component({
-  selector: 'ph-context-menu',
+  selector: "ph-context-menu",
   standalone: false,
-  styleUrls: ['./ph-context-menu.component.scss'],
-  templateUrl: './ph-context-menu.component.html'
+  styleUrls: ["./ph-context-menu.component.scss"],
+  templateUrl: "./ph-context-menu.component.html",
 })
-export class PhContextMenuComponent implements OnInit {
-
-  public isOpened = false;
+export class PhContextMenuComponent implements AfterViewInit {
+  @Input() public entries: Array<{ label: string; action: () => Promise<void> }> = [];
+  @Input() public position: { x: number; y: number } = { x: 0, y: 0 };
 
   constructor(public ref: ElementRef) {
-    window.addEventListener('mousedown', (e) => {  
-      if(!ref.nativeElement.contains(e.target)) {
-        this.close();
-      }
-    });
-
-    window.addEventListener('wheel', (e) => {  
-      if(!ref.nativeElement.contains(e.target)) {
-        this.close();
-      }
-    });
+   
   }
 
-  ngOnInit(): void {
-  }
-
-  public open(position?: {x: number, y: number}) {
-    if(position) {
-      this.ref.nativeElement.style.top = `${position.y - 6}px`;
-      this.ref.nativeElement.style.left = `${position.x - 6}px`;
+  ngAfterViewInit(): void {
+    if (this.position) {
+      this.ref.nativeElement.style.top = `${this.position.y - 6}px`;
+      this.ref.nativeElement.style.left = `${this.position.x - 6}px`;
     }
-    this.ref.nativeElement.style.display = "block";
-    this.isOpened = true;
   }
+
+  public async select(entry: { label: string; action: () => Promise<void> }) {
+    console.log("Selected entry:", entry.label);
+    try {
+      await entry.action();
+    } catch (e) {
+      console.error(e);
+    }
+    
+    this.close();
+  }
+
+  // public open(config:) {
+  //   if(position) {
+  //     this.ref.nativeElement.style.top = `${position.y - 6}px`;
+  //     this.ref.nativeElement.style.left = `${position.x - 6}px`;
+  //   }
+  //   this.ref.nativeElement.style.display = "block";
+  //   this.isOpened = true;
+  // }
 
   public close() {
-    this.isOpened = false;
-    this.ref.nativeElement.style.display = "none";
-  }
 
+  }
 }
