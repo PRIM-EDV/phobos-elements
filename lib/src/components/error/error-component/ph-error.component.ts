68ba0d8161448c1b02617f31a369a6767bd58b13
Lucas Mösch <lucasmoesch@live.de>
2025-08-12 13:56:38 +0200

    Added ph-error component

---
+++ lib/src/components/error/error-component/ph-error.component.ts
@@ -0,0 +1,12 @@
+import { Component, CommonModule } from '@angular/core';
+
+@Component({
+    selector: 'ph-error',
+    standalone: true,
+    imports: [
+        CommonModule
+    ],
+    templateUrl: './ph-error.component.html',
+    styleUrls: ['./ph-error.component.scss']
+})
+export class PhError {}
