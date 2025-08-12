aec8194cafbe531e0617b5a62bb69a2378855f2e
Lucas Mösch <lucasmoesch@live.de>
2025-08-12 15:05:12 +0200

    Fixed broken error import

--- lib/src/components/error/error-component/ph-error.component.ts
+++ lib/src/components/error/error-component/ph-error.component.ts
@@ -1,11 +1,8 @@
-import { Component, CommonModule } from '@angular/core';
+import { Component } from '@angular/core';
 
 @Component({
     selector: 'ph-error',
     standalone: true,
-    imports: [
-        CommonModule
-    ],
     templateUrl: './ph-error.component.html',
     styleUrls: ['./ph-error.component.scss']
 })
